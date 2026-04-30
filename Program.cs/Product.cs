5ce64807947727ce4ff4ff63f237584799317f60
Rapanan-Mavrick-S <rapananmavrick@gmail.com>
2026-04-30 17:49:21 +0800

    Add files via upload

---
+++ Program.cs/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Program
+{
+    class Product
+    {
+        public int ID;
+        public String Name;
+        public double price;
+        public int RemainingStock;
+
+        public void DisplayProduct()
+        {
+            Console.WriteLine($"|{ID}| {Name} - {price} php (Stock: {RemainingStock})");
+        }
+
+        public double GetItemTotal(int quantity)
+        {
+            return price * quantity;
+        }
+
+        public bool HasEnoughStock(int quantity)
+        {
+            return quantity <= RemainingStock;
+        }
+
+        public void DeductStock(int quantity)
+        {
+            RemainingStock -= quantity;
+        }
+    }
+}
